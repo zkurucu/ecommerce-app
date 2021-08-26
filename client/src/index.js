ffc28f88deb0ca878eeaa31100801cc6aa54975b
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-08-26 16:26:19 +0300

    Installation of React Query and take data from api

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./reset.css";
-
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- client/src/pages/Products/index.js
+++ client/src/pages/Products/index.js
@@ -1,13 +1,20 @@
 import Card from "../../components/Card";
 import { Grid } from "@chakra-ui/react";
-
+import {fetchProductList} from "../../api";
+import { useQuery } from "react-query";
 function Products() {
+  const { isLoading, error, data } = useQuery("products", fetchProductList);
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+  console.log(data);
   return (
     <div>
       <Grid templateColumns="repeat(3, 1fr)" gap={4}>
-        <Card />
-        <Card />
-        <Card />
+        {data.map((item, key) => (
+          <Card key={key} item={item} />
+        ))}
       </Grid>
     </div>
   );
