76a0cf38f20110d38661ed06056320d233b0a4b0
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-08-26 15:24:00 +0300

    ChakraUI, ReactRouter installation, Navbar design

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Signin from "./pages/Auth/Signin";
+import Signup from "./pages/Auth/Signup";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <div id="content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+export default App;
