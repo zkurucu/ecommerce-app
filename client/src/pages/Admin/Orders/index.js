f4a6202c234d7f7f4143617873aba832d632ffbc
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-08-31 18:32:20 +0300

    Admin: Order page preparation

--- client/src/pages/Admin/Orders/index.js
+++ client/src/pages/Admin/Orders/index.js
@@ -1,7 +1,56 @@
 import React from "react";
+import { useQuery } from "react-query";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  Text,
+} from "@chakra-ui/react";
+import { fetchOrders } from "../../../api";
 
 function Orders() {
-  return <div>Orders</div>;
+  const { isLoading, isError, data, error } = useQuery(
+    "admin:orders",
+    fetchOrders
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <Text fontSize="2xl" p={5}>
+        Orders
+      </Text>
+      <Table variant="simple">
+        <TableCaption></TableCaption>
+        <Thead>
+          <Tr>
+            <Th>User</Th>
+            <Th>Address</Th>
+            <Th isNumeric>Items</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((item) => (
+            <Tr key={item._id}>
+              <Td>{item.user.email}</Td>
+              <Td>{item.adress}</Td>
+              <Td>{item.items.length}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default Orders;
